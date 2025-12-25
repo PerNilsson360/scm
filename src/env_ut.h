2f9ac13186a12cf3f2a463fb3511c4503c71df28
PerNilsson360 <per@skejt.se>
2025-12-25 14:24:19 +0100

    removing some dead code

--- src/env_ut.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ENV_UT_H_
-#define _ENV_UT_H_
-
-void tst_env_lookup_variable_value();
-void tst_env_extend_environment();
-void tst_env_set_variable_value();
-void tst_env_define_variable();
-
-#endif
